0c6e447adf43cb884a0589b36988848a08415d3d
jingjing <jingjingwangacc@gmail.com>
2025-09-04 12:01:43 -0700

    product-page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import Image from 'next/image'
+import ProductImages from '@/components/ProductImages'
+import CustomizeProducts from '@/components/CustomizeProducts'
+import Add from '@/components/Add'
 
 const SinglePage = () => {
   return (
-    <div>
-      SinglePage
+    <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
+      {/* IMG */}
+      <div className='w-full lg:w-1/2 lg:sticky'>
+        <ProductImages />
+      </div>
+      {/* TEXTS */}
+      <div className='w-full lg:w-1/2 flex flex-col gap-6'>
+        <h1 className='text-4xl font-medium'>Product Name</h1>
+        <p className='text-gray-500'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore corporis, quasi eligendi architecto cumque quas nesciunt sed esse impedit tempora illum necessitatibus quam similique nihil hic inventore repellendus optio modi.
+        </p>
+        <div className='h-[2px] bg-gray-100' />
+        <div className='flex items-center gap-4'>
+          <h3 className='text-xl text-gray-500 line-through'>$59</h3>
+          <h2 className='font-medium text-2xl'>$49</h2>
+        </div>
+        <div className='h-[2px] bg-gray-100' />
+        <CustomizeProducts />
+        <Add />
+        <div className='h-[2px] bg-gray-100' />
+        <div className='text-sm'>
+          <h4 className='font-medium mb-4'>Title</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, vero velit. Ducimus alias aut soluta ad adipisci velit cum facere in animi! Et odio velit ab voluptatem laborum dolorum excepturi?</p>
+        </div>
+        <div className='text-sm'>
+          <h4 className='font-medium mb-4'>Title</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, vero velit. Ducimus alias aut soluta ad adipisci velit cum facere in animi! Et odio velit ab voluptatem laborum dolorum excepturi?</p>
+        </div>
+        <div className='text-sm'>
+          <h4 className='font-medium mb-4'>Title</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, vero velit. Ducimus alias aut soluta ad adipisci velit cum facere in animi! Et odio velit ab voluptatem laborum dolorum excepturi?</p>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/ProductImages.tsx
@@ -0,0 +1,59 @@
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
+
+const images = [
+    {
+        id: 1,
+        url: `https://images.pexels.com/photos/33339522/pexels-photo-33339522.jpeg`
+    },
+    {
+        id: 2,
+        url:'https://images.pexels.com/photos/32406119/pexels-photo-32406119.jpeg'
+    },
+    {
+        id: 3,
+        url:'https://images.pexels.com/photos/33275329/pexels-photo-33275329.jpeg'
+    },
+    {
+        id: 4,
+        url:'https://images.pexels.com/photos/33696492/pexels-photo-33696492.jpeg'
+    },
+]
+
+const ProductImages = () => {
+    const [index, setIndex] = useState(0);
+    return (
+        <div className=''>
+            <div className='h-[500px] relative'>
+                <Image
+                    src={images[index].url}
+                    alt=''
+                    fill
+                    sizes='50vw'
+                    className='object-cover rounded-md'
+                />
+            </div>
+            <div className='flex justify-between gap-4 mt-8 cursor-pointer'>
+                {images.map((img, i) => (
+                    <div 
+                        className='w-1/4 h-32 relative gap-4 mt-8'
+                        key={img.id}
+                        onClick={()=>setIndex(i)}
+                    >
+                    <Image
+                        src={img.url}
+                        alt=''
+                        fill
+                        sizes='20vw'
+                        className='object-cover rounded-md'
+                    />
+                </div>
+                ))}
+
+            </div>
+        </div>
+    )
+}
+
+export default ProductImages
